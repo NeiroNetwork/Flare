aeb49a8dc7c30906a91927ed55007d702aceb078
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-08 00:54:22 +0900

    Add `NumericalSampling`

---
+++ src/utils/NumericalSampling.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\Flare\utils;
+
+use Countable;
+
+class NumericalSampling implements Countable {
+
+	public function count(): int {
+		return count($this->list);
+	}
+
+	/**
+	 * @var float[]
+	 */
+	protected array $list;
+
+	/**
+	 * @var int
+	 */
+	public int $max;
+
+	public static function fromArray(array $ar, int $max = -1) {
+		$s = new static($max);
+		foreach ($ar as $v) {
+			$s->add($v);
+		}
+		return $s;
+	}
+
+	public function __construct(int $max = -1) {
+		$this->list = [];
+		$this->max = $max;
+	}
+
+	public function add(float $sample) {
+		array_unshift($this->list, $sample);
+		if ($this->max > 0) {
+			if (count($this->list) > $this->max) {
+				array_pop($this->list);
+			}
+		}
+	}
+
+	/**
+	 * @return float[]
+	 */
+	public function getAll(): array {
+		return $this->list;
+	}
+
+	public function clear(): void {
+		$this->list = [];
+	}
+
+	public function isMax(): bool {
+		return count($this->list) >= $this->max;
+	}
+
+	public function getFirst(): float {
+		return $this->list[array_key_first($this->list)];
+	}
+
+	public function getLast(): float {
+		return $this->list[array_key_last($this->list)];
+	}
+}
