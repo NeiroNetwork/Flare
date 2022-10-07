cd349cbb1746bcbbd8cc832847a7075bf898ace3
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-08 00:58:49 +0900

    Add: `CheckGroup`

---
+++ src/profile/check/CheckGroup.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\Flare\profile\check;
+
+final class CheckGroup {
+
+	const COMBAT = 0;
+	const MOVEMENT = 1;
+	const MISC = 2;
+	const PACKET = 3;
+	const DEBUG = 4;
+
+	const UNKNOWN = -1;
+}
