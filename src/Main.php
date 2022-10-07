384329ed3b7983d40544cd5b2bb332b3f898ef97
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-08 00:55:32 +0900

    Flare AntiCheat Plugin

--- src/Main.php
+++ src/Main.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\Flare;
 
+use NeiroNetwork\Flare\network\NACKHandler;
+use pocketmine\network\mcpe\raklib\RakLibInterface;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
 
+class Main extends PluginBase {
+
+	protected Flare $flare;
+
+	protected function onEnable(): void {
+		$this->flare = new Flare($this);
+
+		$this->flare->start();
+	}
 }
