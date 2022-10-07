a5adfc641942520a761bbc131eeb997e36baa5eb
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-08 00:58:00 +0900

    Interface: `ICheck`

---
+++ src/profile/check/ICheck.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\Flare\profile\check;
+
+interface ICheck {
+
+	/**
+	 * @return string
+	 * 
+	 * check name
+	 */
+	public function getName(): string;
+
+	/**
+	 * @return int
+	 * 
+	 * todo: return CheckGroup instance
+	 */
+	public function getCheckGroup(): int;
+
+	public function fail(FailReason $reason): void;
+
+	public function getFullId(): string;
+
+	public function onEnable(): void;
+
+	public function onDisable(): void;
+
+	public function onLoad(): void;
+
+	public function setEnabled(bool $enabled = true): void;
+
+	public function isEnabled(): bool;
+}
