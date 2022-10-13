8aadfdbfb0a7e1725c8643479ee9707b2911cdec
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-13 22:39:57 +0900

    ClassNameAsCheckIdTrait - 修正

--- src/profile/check/ClassNameAsCheckIdTrait.php
+++ src/profile/check/ClassNameAsCheckIdTrait.php
@@ -11,9 +11,25 @@ trait ClassNameAsCheckIdTrait {
 
 	private function solve(): void {
 		$ref = new \ReflectionClass($this);
-		$words =  explode(",", wordwrap($ref->getShortName(), 75, ","));
-		$this->name = $words[0] ?? throw new \Exception("cannot solve check name from class name: \"{$ref->getShortName()}\"");
-		$this->type = $words[1] ?? "";
+		$camelcase = lcfirst($ref->getShortName());
+		$index = false;
+
+		// fixme: preg_match?
+		foreach (range("A", "Z") as $needle) { // stupid
+			$index = strpos($camelcase, $needle);
+			if ($index !== false) {
+				break;
+			}
+		}
+		if ($index !== false) {
+			$chunks = substr_replace($camelcase, ",", $index, 0);
+			$words = explode(",", $chunks);
+			$this->name = ucfirst($words[0] ?? throw new \Exception("cannot solve check name from class name: \"{$ref->getShortName()}\""));
+			$this->type = $words[1] ?? "";
+		} else {
+			$this->name = $ref->getShortName();
+			$this->type = "";
+		}
 	}
 
 	public function getName(): string {
