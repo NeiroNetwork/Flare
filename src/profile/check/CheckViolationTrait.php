b2fe201069dd375690380dc1ebf6cb5dc6a0c349
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-29 22:04:44 +0900

    Violation Percentage が 100%を超える問題を修正

--- src/profile/check/CheckViolationTrait.php
+++ src/profile/check/CheckViolationTrait.php
@@ -6,7 +6,7 @@ namespace NeiroNetwork\Flare\profile\check;
 
 trait CheckViolationTrait {
 
-	protected int $vl = 0;
+	private int $vl = 0;
 
 	protected int $punishVl = (100 * 15);
 
@@ -19,10 +19,38 @@ trait CheckViolationTrait {
 	}
 
 	public function violate(): void {
-		$this->vl += 100;
+		$this->vl = min($this->punishVl, $this->vl + 100);
 	}
 
 	public function reward(): void {
 		$this->vl = max(0, $this->vl - 1);
+	}
+
+
+	/**
+	 * Set the value of punishVl
+	 *
+	 * @param int $punishVL
+	 *
+	 * @return self
+	 */
+	public function setPunishVL(int $punishVL): self {
+		$this->punishVl = $punishVL;
+
+		return $this;
+	}
+
+
+	/**
+	 * Set the value of vl
+	 *
+	 * @param int $vl
+	 *
+	 * @return self
+	 */
+	public function setVL(int $vl): self {
+		$this->vl = $vl;
+
+		return $this;
 	}
 }
