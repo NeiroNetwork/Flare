cdec2f2c4c7159106cf6856eb625bf609463e630
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-03-15 20:16:08 +0900

    例外処理の忘れを修正

--- src/profile/check/list/combat/reach/ReachB.php
+++ src/profile/check/list/combat/reach/ReachB.php
@@ -8,21 +8,11 @@ use NeiroNetwork\Flare\event\player\PlayerAttackEvent;
 use NeiroNetwork\Flare\profile\check\BaseCheck;
 use NeiroNetwork\Flare\profile\check\CheckGroup;
 use NeiroNetwork\Flare\profile\check\ClassNameAsCheckIdTrait;
-use NeiroNetwork\Flare\profile\check\HandleInputPacketCheck;
-use NeiroNetwork\Flare\profile\check\HandleInputPacketCheckTrait;
 use NeiroNetwork\Flare\profile\check\ViolationFailReason;
 use NeiroNetwork\Flare\profile\data\ActionNotifier;
 use NeiroNetwork\Flare\profile\data\ActionRecord;
-use NeiroNetwork\Flare\profile\data\MovementData;
-use NeiroNetwork\Flare\utils\Math;
 use pocketmine\entity\Entity;
-use pocketmine\event\EventPriority;
-use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Vector3;
-use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
-use pocketmine\network\mcpe\protocol\SpawnParticleEffectPacket;
-use pocketmine\network\mcpe\protocol\types\DimensionIds;
-use SplFixedArray;
 
 class ReachB extends BaseCheck {
 	use ClassNameAsCheckIdTrait;
@@ -36,13 +26,17 @@ class ReachB extends BaseCheck {
 
 		$notifier = new ActionNotifier();
 		$notifier->notifyEnd(function (ActionRecord $record): void {
-			$this->handleTriggerAim();
+			if ($this->tryCheck()) $this->handleTriggerAim();
 		});
 
 		$this->profile->getCombatData()->getTriggerAimRecord()->notify($notifier);
 	}
 
 	public function onUnload(): void {
+	}
+
+	public function isExperimental(): bool {
+		return true;
 	}
 
 	public function handleTriggerAim(): void {
@@ -57,8 +51,8 @@ class ReachB extends BaseCheck {
 
 			$reach = $pos->distanceSquared($aimingAt);
 
-			if ($reach > 9.0 + 4.0) {
-				$this->fail(new ViolationFailReason("Reach: {$reach}"));
+			if ($reach > 9.0 + 0.015) { // epsilon or math error
+				$this->fail(new ViolationFailReason("Aim Reach: {$reach}"));
 			}
 
 			$this->broadcastDebugMessage((string) $reach);
