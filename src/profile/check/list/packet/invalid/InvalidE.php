4c08cd5e630bd679e1c997314b2c4136f3a8df7d
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-03-27 13:54:06 +0900

    InvalidE 追加

--- src/profile/check/list/packet/invalid/InvalidE.php
+++ src/profile/check/list/packet/invalid/InvalidE.php
@@ -9,20 +9,37 @@ use NeiroNetwork\Flare\profile\check\CheckGroup;
 use NeiroNetwork\Flare\profile\check\ClassNameAsCheckIdTrait;
 use NeiroNetwork\Flare\profile\check\HandleInputPacketCheck;
 use NeiroNetwork\Flare\profile\check\HandleInputPacketCheckTrait;
+use NeiroNetwork\Flare\profile\check\ViolationFailReason;
 use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
+use pocketmine\network\mcpe\protocol\types\PlayerAuthInputFlags;
 
 class InvalidE extends BaseCheck implements HandleInputPacketCheck {
 	use ClassNameAsCheckIdTrait;
 	use HandleInputPacketCheckTrait;
 
+	protected ?int $lastClientTick = null;
+
 	public function getCheckGroup(): int {
 		return CheckGroup::PACKET;
+	}
+
+	public function isExperimental(): bool {
+		return true;
 	}
 
 	public function handle(PlayerAuthInputPacket $packet): void {
 		$this->reward();
 		$player = $this->profile->getPlayer();
 
-		$ki = $this->profile->getKeyInputs();
+		if (is_null($this->lastClientTick)) {
+			$this->lastClientTick = $packet->getTick();
+			return;
+		}
+
+		if ($packet->getTick() - $this->lastClientTick !== 1) {
+			$this->fail(new ViolationFailReason("Invalid auth tick"));
+		}
+
+		$this->lastClientTick = $packet->getTick();
 	}
 }
