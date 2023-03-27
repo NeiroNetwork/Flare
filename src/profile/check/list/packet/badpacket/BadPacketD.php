fe74de1e69bbdca65bad4b40eb537666c024ea7c
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-03-27 13:53:38 +0900

    BadPacketD 追加

---
+++ src/profile/check/list/packet/badpacket/BadPacketD.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\Flare\profile\check\list\packet\badpacket;
+
+use NeiroNetwork\Flare\event\player\PlayerAttackEvent;
+use NeiroNetwork\Flare\profile\check\BaseCheck;
+use NeiroNetwork\Flare\profile\check\CheckGroup;
+use NeiroNetwork\Flare\profile\check\ClassNameAsCheckIdTrait;
+use NeiroNetwork\Flare\profile\check\HandleInputPacketCheck;
+use NeiroNetwork\Flare\profile\check\HandleInputPacketCheckTrait;
+use NeiroNetwork\Flare\profile\check\ViolationFailReason;
+use pocketmine\event\EventPriority;
+use pocketmine\network\mcpe\protocol\PlayerActionPacket;
+use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
+use pocketmine\network\mcpe\protocol\types\PlayerAction;
+
+class BadPacketD extends BaseCheck implements HandleInputPacketCheck {
+	use ClassNameAsCheckIdTrait;
+	use HandleInputPacketCheckTrait;
+
+	public function getCheckGroup(): int {
+		return CheckGroup::PACKET;
+	}
+
+	public function handle(PlayerAuthInputPacket $packet): void {
+		$this->reward();
+		$player = $this->profile->getPlayer();
+
+		$md = $this->profile->getMovementData();
+		$ki = $this->profile->getKeyInputs();
+
+		$epsilon = 0.000001;
+		if (abs($packet->getMoveVecX()) > 1 + $epsilon || abs($packet->getMoveVecZ()) > 1 + $epsilon) {
+			$this->fail(new ViolationFailReason("Invalid Move Vec"));
+		}
+	}
+}
